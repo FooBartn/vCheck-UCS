1e559d9f200de5bf2a1fdbb30f2ac656259442c1
FooBartn <joshua.r.barton@gmail.com>
2016-03-13 17:14:39 -0500

    Added 2 Plugins

---
+++ Plugins/30 Server Hardware/02 Power Policy.ps1
@@ -0,0 +1,23 @@
+# Start of Settings
+# Define power redundancy setting (non-redundant/n+1/grid):
+$PowerRedundancy = 'grid'
+# End of Settings
+
+$UcsPowerPolicyTable = @()
+$UcsPowerPolicy = Get-UcsPowerControlPolicy
+
+If($UcsPowerPolicy.Redundancy -ne $PowerRedundancy) {
+    $Details = '' | Select Redundancy
+    $Details.Redundancy = $UcsPowerPolicy.Redundancy
+    $UcsPowerPolicyTable += $Details    
+}
+
+$UcsPowerPolicyTable
+
+$Title = "Power Policy"
+$Header =  "Power Policy"
+$Comments = "Preferred Power Policy Setting -- Redundancy: $PowerRedundancy"
+$Display = "Table"
+$Author = "Joshua Barton"
+$PluginVersion = 1.0
+$PluginCategory = "UCS"
